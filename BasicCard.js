2fd472841da379f3f803a325d6b8b02683b1d07a
Steven Cadwallader <steven.caddie@gmail.com>
2017-09-17 19:59:58 -0500

    9/17 8:00

--- BasicCard.js
+++ BasicCard.js
@@ -1,6 +1,36 @@
-function basicCards(front, back){
+var inquirer = require('inquirer');
+var fs = require('fs');
+var loop = 0;
+var frontCard = [];
+var backCard = [];
+
+function BasicCard(front, back){
 	this.front = front;
 	this.back = back;
+	this.printInfo = function(){
+		console.log("Question: "+this.front+"\nAnswer: "+this.back);
+	}
 }
 
-module.exports = BasicCard;
+var getFront = function(){
+	if (loop < 2){
+		inquirer.prompt([{
+			name:"front",
+			message:"Enter the question for this card: "
+		},{
+			name:"back",
+			message:"Enter the answer for the card: "
+		}]).then(function(answers){
+			var newCard = new BasicCard(answers.front, answers.back);
+			newCard.printInfo();
+			loop++;
+			getFront();
+		})
+	} 
+}
+
+getFront();
+
+
+
+module.exports = BasicCard; 
---
+++ flashcard.js
@@ -0,0 +1,4 @@
+var inquirer = require('inquirer');
+var fs = require('fs');
+var BasicCard = require("./BasicCard.js");
+var ClozeCard = require("./ClozeCard.js");
